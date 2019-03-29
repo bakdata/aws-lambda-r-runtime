2751c32866c248540249e02eec51c077d3f47abf
Philipp Schirmer <philipp.schirmer@bakdata.com>
2019-03-29 14:54:50 +0100

    Stop lambda server before running a new one

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ -f sam.pid ]; then
+  ./stop_server.sh
+fi
 sam local start-lambda --parameter-overrides 'ParameterKey=RuntimeLayer,ParameterValue='"$1"' ParameterKey=RecommendedLayer,ParameterValue='"$2"' ParameterKey=AWSLayer,ParameterValue='"$3"'' &
 echo $! > sam.pid
 sleep 10
