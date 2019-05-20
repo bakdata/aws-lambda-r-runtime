c8e7b6f14fb73f27006df99141b5f45c66a805e2
Philipp Schirmer <philipp.schirmer@bakdata.com>
2019-05-20 11:35:40 +0200

    Remove support for ap-east-1 region

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,6 @@ function releaseToRegion {
 
 regions=(us-east-1 us-east-2
          us-west-1 us-west-2
-         ap-east-1
          ap-south-1
          ap-northeast-1 ap-northeast-2
          ap-southeast-1 ap-southeast-2
