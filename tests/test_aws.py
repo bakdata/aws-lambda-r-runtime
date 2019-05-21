aa078c48f1328d3b5ec4845625a96b25ff68b85e
Philipp Schirmer <philipp.schirmer@bakdata.com>
2019-05-21 14:35:41 +0200

    Revert to default log level

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -11,26 +11,27 @@ class TestAWSLayer(unittest.TestCase):
     def setUpClass(cls):
         cls.lambda_server = start_local_lambda()
 
-    # def test_s3_get_object(self):
-    #     lambda_client = self.lambda_server.get_client()
-    #     response = lambda_client.invoke(FunctionName="AWSFunction")
-    #     payload = response['Payload'].read().decode('utf-8')
-    #     result = json.loads(payload)['result']
-    #     self.assertEqual(len(result), 1)
-    #     self.assertDictEqual(result[0], {
-    #         "DRG.Definition": "039 - EXTRACRANIAL PROCEDURES W/O CC/MCC",
-    #         "Provider.Id": "10001",
-    #         "Provider.Name": "SOUTHEAST ALABAMA MEDICAL CENTER",
-    #         "Provider.Street.Address": "1108 ROSS CLARK CIRCLE",
-    #         "Provider.City": "DOTHAN",
-    #         "Provider.State": "AL",
-    #         "Provider.Zip.Code": 36301,
-    #         "Hospital.Referral.Region.Description": "AL - Dothan",
-    #         "Total.Discharges": 91,
-    #         "Average.Covered.Charges": "$32963.07",
-    #         "Average.Total.Payments": "$5777.24",
-    #         "Average.Medicare.Payments": "$4763.73"
-    #     })
+    @unittest.skip
+    def test_s3_get_object(self):
+        lambda_client = self.lambda_server.get_client()
+        response = lambda_client.invoke(FunctionName="AWSFunction")
+        payload = response['Payload'].read().decode('utf-8')
+        result = json.loads(payload)['result']
+        self.assertEqual(len(result), 1)
+        self.assertDictEqual(result[0], {
+            "DRG.Definition": "039 - EXTRACRANIAL PROCEDURES W/O CC/MCC",
+            "Provider.Id": "10001",
+            "Provider.Name": "SOUTHEAST ALABAMA MEDICAL CENTER",
+            "Provider.Street.Address": "1108 ROSS CLARK CIRCLE",
+            "Provider.City": "DOTHAN",
+            "Provider.State": "AL",
+            "Provider.Zip.Code": 36301,
+            "Hospital.Referral.Region.Description": "AL - Dothan",
+            "Total.Discharges": 91,
+            "Average.Covered.Charges": "$32963.07",
+            "Average.Total.Payments": "$5777.24",
+            "Average.Medicare.Payments": "$4763.73"
+        })
 
     @classmethod
     def tearDownClass(cls):
