fa9feaceb7df497c42ffc2361f3fd00f81639d0b
Philipp Schirmer <philipp.schirmer@bakdata.com>
2019-06-18 08:59:40 +0200

    Only skip AWS test if pull request from different repo

--- tests/travis.py
+++ tests/travis.py
@@ -2,4 +2,5 @@ import os
 
 
 def is_pull_request() -> bool:
-    return os.getenv('TRAVIS_PULL_REQUEST') == 'true'
+    return os.getenv('TRAVIS_PULL_REQUEST') == 'true' \
+           and os.getenv('TRAVIS_PULL_REQUEST_SLUG') != os.getenv('TRAVIS_REPO_SLUG')
