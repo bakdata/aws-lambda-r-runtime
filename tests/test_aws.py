eaa901b2dfc17f65c80fbf22ca63746b57dc70f6
Philipp Schirmer <philipp.schirmer@bakdata.com>
2020-01-29 15:18:38 +0000

    Downgrade awspack to aws.s3

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -20,8 +20,7 @@ class TestAWSLayer(unittest.TestCase):
     def get_client(self):
         return self.lambda_server.get_client() if is_local() else boto3.client('lambda')
 
-#    @unittest.skipUnless(is_local(), "Credentials missing for remote Lambda")
-    @unittest.skip('awspack has been removed from cran')
+    @unittest.skipUnless(is_local(), "Credentials missing for remote Lambda")
     def test_s3_get_object(self):
         lambda_client = self.get_client()
         response = lambda_client.invoke(FunctionName=get_function_name("AWSFunction"))
